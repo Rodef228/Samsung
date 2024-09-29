1dce660d4242be54c8331b5e67a82f75b67dd661
unknown <fedor.r.fomin@gmail.com>
2024-09-29 13:41:17 +0300

    Initial commit

---
+++ Module1/day4/a.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int A = scanner.nextInt();
+        int i = scanner.nextInt();
+        
+        int result = A & (~((1 << i) - 1));
+        
+        System.out.println(result);
+    }
+}
