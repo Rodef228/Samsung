4f60276fa33e64ab44a72a1c53b4eda46e7088c6
unknown <fedor.r.fomin@gmail.com>
2024-09-29 15:40:52 +0300

    Initial commit

--- Module1/day4/BitReset.java
+++ Module1/day4/BitReset.java
@@ -4,10 +4,10 @@ public class BitReset {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        int A = scanner.nextInt();
+        long A = scanner.nextInt();
         int i = scanner.nextInt();
         
-        int result = A & (~((1 << i) - 1));
+        long result = A & (~((1 << i) - 1));
         
         System.out.println(result);
     }
